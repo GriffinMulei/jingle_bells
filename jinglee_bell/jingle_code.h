b1462290e771307b1b50bd92320fea0fb3aaccdd
GriffinMulei <muleigriffin@gmail.com>
2023-12-04 03:51:45 -0800

    jingle_bells

---
+++ jinglee_bell/jingle_code.h
@@ -0,0 +1,15 @@
+// jingle_code.h
+
+#ifndef JINGLE_CODE_H
+#define JINGLE_CODE_H
+
+void print_decorated_colored_line(const char *line, int color_code);
+
+void jingle_bells();
+void dashing_through_the_code();
+void oh_what_fun();
+void chorus_repeat();
+void entire_song();
+
+#endif // JINGLE_CODE_H
+
